0f2e8457ceff8cbee024266c6332fef414e84174
monki1 <kxye111@gmail.com>
2023-02-14 12:45:25 -0500

    added build context reciever

--- lib/quick_app_generator.dart
+++ lib/quick_app_generator.dart
@@ -1,54 +1,61 @@
-
 import 'package:flutter/material.dart';
 
-
 ThemeData defaultTheme = ThemeData.dark().copyWith(
-    scaffoldBackgroundColor: Colors.black,
-    textTheme: const TextTheme(
-      bodySmall: TextStyle(
-        color: Colors.white,
-        fontSize: 20,
-      ),
-      bodyMedium: TextStyle(
-        color: Colors.white,
-        fontSize: 30,
-      ),
-      bodyLarge: TextStyle(
-        color: Colors.white,
-        fontSize: 40,
-      ),
-      titleLarge: TextStyle(
-        color: Colors.white,
-        fontSize: 40,
-      ),
-      titleSmall: TextStyle(
-        color: Colors.white,
-        fontSize: 20,
-      ),
-      titleMedium: TextStyle(
-        color: Colors.white,
-        fontSize: 30,
-      ),
+  scaffoldBackgroundColor: Colors.black,
+  textTheme: const TextTheme(
+    bodySmall: TextStyle(
+      color: Colors.white,
+      fontSize: 20,
     ),
-  );
+    bodyMedium: TextStyle(
+      color: Colors.white,
+      fontSize: 30,
+    ),
+    bodyLarge: TextStyle(
+      color: Colors.white,
+      fontSize: 40,
+    ),
+    titleLarge: TextStyle(
+      color: Colors.white,
+      fontSize: 40,
+    ),
+    titleSmall: TextStyle(
+      color: Colors.white,
+      fontSize: 20,
+    ),
+    titleMedium: TextStyle(
+      color: Colors.white,
+      fontSize: 30,
+    ),
+  ),
+);
 
-Widget quickAppGenerator({required Widget screen, ThemeData? theme, Function(BuildContext)? buildContextReceiver}){
-  return QuickApp(page: QuickPage(body: screen), theme_: theme, buildContextReceiver: buildContextReceiver);
+Widget quickAppGenerator(
+    {required Widget screen,
+    ThemeData? theme,
+    Function(BuildContext)? buildContextReceiver}) {
+  return QuickApp(
+      page: QuickPage(body: screen),
+      theme_: theme,
+      buildContextReceiver: buildContextReceiver);
 }
-
 
 class QuickApp extends StatelessWidget {
   Function(BuildContext)? _buildContextReceiver;
   final StatelessWidget page;
   ThemeData theme = defaultTheme;
-  
-  QuickApp({required this.page ,super.key, ThemeData? theme_, 
-    /// This is a function that will be called with the build context of the app.
-    Function(BuildContext)? buildContextReceiver}){
-    if(theme_ != null){
+
+  QuickApp(
+      {required this.page,
+      super.key,
+      ThemeData? theme_,
+
+      /// This is a function that will be called with the build context of the app.
+      Function(BuildContext)? buildContextReceiver}) {
+    if (theme_ != null) {
       theme = theme_;
     }
-    if(buildContextReceiver != null){
+    if (buildContextReceiver != null) {
       _buildContextReceiver = buildContextReceiver;
     }
   }
@@ -56,7 +63,7 @@ class QuickApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    if(_buildContextReceiver != null){
+    if (_buildContextReceiver != null) {
       _buildContextReceiver!(context);
     }
     return MaterialApp(
@@ -67,17 +74,15 @@ class QuickApp extends StatelessWidget {
   }
 }
 
-class QuickPage extends StatelessWidget{
+class QuickPage extends StatelessWidget {
   final Widget body;
   const QuickPage({required this.body, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: body,
-      )
-    );
+        body: SafeArea(
+      child: body,
+    ));
   }
-
 }
